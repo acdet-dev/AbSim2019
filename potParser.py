e71bfed371259b0d6fb796fe56eea3db0461ddb1
seanald15d <s.mccullough@tcu.edu>
2020-04-24 09:53:44 -0500

    Adding pot parser utility to VC

---
+++ potParser.py
@@ -0,0 +1,13 @@
+with open("C:\\Users\\Steve\\PyCharmProjects\\AbSim2019\\absim_strings.pot", "r+") as f1:
+	s_list = f1.readlines()
+	substring = 'msgid "'
+	for i in range(0, len(s_list)):
+		if substring in s_list[i]:
+			print(s_list[i+1])
+			a = s_list[i+1].replace('""', '"' + s_list[i][7:-2] + '"')
+			s_list[i+1] = a
+
+with open("C:\\Users\\Steve\\PyCharmProjects\\AbSim2019\\i18n\\abtemplate.txt", "w") as f2:
+	f2.writelines(s_list)
+f1.close()
+f2.close()
